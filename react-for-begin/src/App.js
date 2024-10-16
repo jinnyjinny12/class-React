d55f6ef101dd37ae401361476957148609375413
jinny <eujin.mstory@gmail.com>
2024-10-16 12:40:03 +0900

    lecture 6.2

--- react-for-begin/src/App.js
+++ react-for-begin/src/App.js
@@ -1,12 +1,15 @@
-import Button from "./Button";
-import styles from "./App.module.css";
-
+import { useEffect, useState } from "react";
 
 function App() {
+  const [counter, setValue] = useState(0);
+  const onClick = () => setValue((prev) => prev + 1);
+  console.log("Run all the time");
+  useEffect(() => { console.log("i run only once"); }, []);
   return (
     <div>
-     <h1 className={styles.title}>Welcome Back!</h1>
-     <Button text={"Continue"} />
+     <h1>{counter}</h1>
+    <button onClick={onClick}>click me</button>
+    
     </div>
   );
 }
