d651b66ac6f2f39d5e24a6b6823e02f1b992eaea
jinny <eujin.mstory@gmail.com>
2024-10-16 13:06:56 +0900

    lecture 6.2

--- react-for-begin/src/App.js
+++ react-for-begin/src/App.js
@@ -2,11 +2,28 @@ import { useEffect, useState } from "react";
 
 function App() {
   const [counter, setValue] = useState(0);
+  const [keyword, setKeyword] = useState("");
   const onClick = () => setValue((prev) => prev + 1);
-  console.log("Run all the time");
-  useEffect(() => { console.log("i run only once"); }, []);
+  const onChange = (event) => setKeyword(event.target.value);
+  
+  useEffect(() => { console.log("I run only once"); }, []);
+  useEffect(() => {
+    console.log("I run Keyword changes");
+  }, [keyword]);
+  useEffect(() => {
+    console.log("I run counter changes");
+  }, [counter]);
+  useEffect(() => {
+    console.log("I run keyword & counter changes");
+  }, [keyword, counter]);
   return (
     <div>
+    <input 
+    value={keyword}
+    onChange={onChange} 
+    type="text" 
+    placeholder="Search here" />
+
      <h1>{counter}</h1>
     <button onClick={onClick}>click me</button>
     
