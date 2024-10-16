1408c200ddc031c78b29dcba04b2e8f09d7387e3
jinny <eujin.mstory@gmail.com>
2024-10-16 15:13:42 +0900

    lecture 7.1

--- react-for-todo/src/App.js
+++ react-for-todo/src/App.js
@@ -1,23 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+
 
 function App() {
+const [toDo, setToDo] = useState("");
+const [toDos, setToDos] = useState([]);
+const onChange = (event) => setToDo(event.target.value);
+// 엔터를 치면 내용이 초기화된다 
+const onSubmit = (event) => {
+  event.preventDefault()
+  if (toDo === "") {
+    return;
+  }
+  setToDo("");
+  setToDos(
+    (currentArray) => [toDo, ...currentArray]);
+};
+console.log(toDos); 
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>My toDos ({toDo.length}) </h1>
+      <form onSubmit={onSubmit}>
+        <input
+          onChange={onChange}
+          value={toDo}
+          type="text"
+          placeholder="Write your to do.."
+        
+         />
+        <button>Add To Do</button>   
+      </form>
+      <hr />
+      <ul>
+      {toDos.map((item, index) => 
+      (<li key={index}>{item}</li>))}
+      </ul>
+      
+     
     </div>
   );
 }
